1976f18905c39ac9f6c4234bb4349769c18a4bd7
Chadx4292 <accounts@chadx.app>
2025-06-19 23:32:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿document.getElementById('mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+    const button = document.getElementById('mode-toggle');
+    button.textContent = document.body.classList.contains('dark-mode') ? '🌙' : '☀️';
+});
